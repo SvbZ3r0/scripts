2f3ec922ea2889e6329e13d2bb3be7471e8e9237
Gughan Ravikumar <57280279+GughanRk@users.noreply.github.com>
2020-07-09 17:16:52 -0700

    Checks for Windows

--- cputemp.py
+++ cputemp.py
@@ -1,8 +1,15 @@
 import os
 
+if os.name != 'nt':
+	print('This script currently supports Windows only')
+	exit(1)
+
 def cputemp():
-	with os.popen('cputemp.bat') as fh:
-		return fh.read()
+	cmd = 'wmic /namespace:\\\\root\\wmi PATH MSAcpi_ThermalZoneTemperature get CurrentTemperature /value'
+	with os.popen(cmd) as fh:
+		val = fh.read().split('=')[-1]
+	val = (int(val) - 2731.5) / 10			# convert mK to °C
+	return str(val)
 
 if __name__ == '__main__':
-	print(cputemp().strip())
+	print(cputemp() + '°C')
