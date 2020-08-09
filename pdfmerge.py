f4f44076e4a8c8e66ea87d856d9030a02ce9c515
Gughan Ravikumar <57280279+GughanRk@users.noreply.github.com>
2020-08-08 20:59:38 -0700

    Clean up

--- pdfmerge.py
+++ pdfmerge.py
@@ -11,7 +11,6 @@ class App(QMainWindow):
 		super().__init__()
 		self.title = 'PDFMerger'
 		self.setWindowTitle(self.title)
-		self.setGeometry(self.left, self.top, self.width, self.height)
 
 		self.table_widget = UIWindow(self)
 		self.setCentralWidget(self.table_widget)
