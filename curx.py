5ccab56e28890e9411e6f06213b8acffd0fc498b
Gughan Ravikumar <57280279+SvbZ3r0@users.noreply.github.com>
2020-12-15 15:22:42 -0800

    Rewrite

--- curx.py
+++ curx.py
@@ -18,12 +18,119 @@ except ImportError:
 			print("Please enter a valid number", file=sys.stderr)
 			exit(1)
 
-with open(os.path.join(os.path.dirname(sys.argv[0]), 'data', 'curx.conf')) as f:
-	conf = json.load(f)
-APP_KEY = conf['APP_KEY']
-URL = 'https://openexchangerates.org/api/{0}?app_id={1}&base=USD&show_alternative=true&prettyprint=false'.format('{0}', APP_KEY)
-cache_file = os.path.join(os.path.dirname(sys.argv[0]), 'data', 'rates.json')
-rates = dict()
+class CurrencyExchange():
+	def __init__(self):
+		# self.from_curr, self.to_curr, self.val = args
+		self.data = dict()
+		with open(os.path.join(os.path.dirname(sys.argv[0]), 'data', 'curx.conf')) as f:
+			conf = json.load(f)
+		self.URL = 'https://openexchangerates.org/api/{0}?app_id={1}&base=USD&show_alternative=true&prettyprint=false'.format('{0}', conf['APP_KEY'])
+		self.cache_file = os.path.join(os.path.dirname(sys.argv[0]), 'data', 'rates.json')
+		# create 'data' folder if it doesn't already exist
+		Path(os.path.dirname(self.cache_file)).mkdir(parents=True, exist_ok=True)
+		self.load_rates()
+
+	def write_to_file(self):
+		try:
+			with open(self.cache_file, 'w', encoding='utf-8') as cache:
+				json.dump(self.data, cache, indent=4)
+		except IOError:
+			print('Unable to cache results.', file=sys.stderr)
+			pass
+
+	def load_rates(self):
+		try:
+			with open(self.cache_file, 'r', encoding='utf-8') as cache:
+				self.data = json.load(cache)
+		except FileNotFoundError:
+			self.create_cache()
+			return
+		time_since_last_update = datetime.datetime.today() - datetime.datetime.strptime(self.data['date'], '%a, %d %b %Y %H:%M:%S %Z')
+		if time_since_last_update > datetime.timedelta(days=1):
+			print('Updating rates')
+			self.get_current_rates()
+
+	def get_current_rates(self, overwrite = False):
+		url = self.URL.format('latest.json')
+		try:
+			r = requests.get(url)
+			r.raise_for_status()
+		except ConnectionError:
+			print('Unable to update exchange rates.', file=sys.stderr)
+			return
+		except:
+			# TODO change this
+			print('Unable to update exchange rates.', file=sys.stderr)
+			return
+		data = self.data
+		data['date'] = r.headers['Last-Modified']
+		_rates = r.json()
+		data['base'] = _rates['base']
+		_rates = _rates['rates']
+		# TODO add functionality for help option
+		for k,v in _rates.items():
+			data['rates'][k]['rate'] = v
+		self.data = data
+		self.write_to_file()
+
+	def create_cache(self):
+		url = self.URL.format('currencies.json')
+		try:
+			r = requests.get(url)
+			r.raise_for_status()
+		except:
+			# TODO
+			print('Unable to get exhange rates', file=sys.stderr)
+			exit(1)
+		print(r.text)
+		currencies = json.loads(r.text)
+		# data['date'] = datetime.datetime.strftime(datetime.datetime.today(), '%Y-%m-%d')
+		data['date'] = r.headers['Last-Modified']	# format '%a, %d %b %Y %H:%M:%S %Z'
+		data['base'] = 'USD'
+		data['rates'] = dict()
+		for k,v in currencies.items():
+			data['rates'][k] = {'name': v}
+		data['etag'] = r.headers['Etag']
+		url = self.URL.format('latest.json')
+		try:
+			r = requests.get(url)
+			r.raise_for_status()
+		except:
+			# TODO
+			pass
+		rates = json.loads(r.text)['rates']
+		for k,v in currencies.items():
+			data['rates'][k]['rate'] = rates[k]
+		self.data = data
+		self.write_to_file()
+
+	def convert(self, from_curr, to_curr, val):
+		rate = 0
+		base = self.data['base']
+		rates = self.data['rates']
+		if from_curr == base:
+			try:
+				rate = rates[to_curr]['rate']
+			except KeyError:
+				print(f'Unknown currency: {to_curr}', file=sys.stderr)
+				exit(1)
+		elif from_curr not in rates:
+			print(f'Unknown currency: {from_curr}', file=sys.stderr)
+			exit(1)
+		if to_curr == base:
+			rate = 1 / rates[from_curr]['rate']
+		elif to_curr not in rates:
+			print(f'Unknown currency: {to_curr}', file=sys.stderr)
+			exit(1)
+		if not rate:
+			rate = rates[to_curr]['rate'] / rates[from_curr]['rate']
+		converted = f'{val * rate :.2f}'
+		fmt = 'i' if to_curr == 'INR' else 'w'
+		converted = conv(converted, fmt)
+		if 'symbol' in rates[to_curr].keys():
+			converted = rates[to_curr]['symbol'] + ' ' + converted
+		return converted
+
 
 def curx_man():
 	# TODO
@@ -37,117 +144,8 @@ def parse_args():
 	args = parser.parse_args()
 	return args.from_.upper(), args.to.upper(), float(conv(args.value))
 
-def create_cache():
-	url = URL.format('currencies.json')
-	try:
-		r = requests.get(url)
-		r.raise_for_status()
-	except:
-		# TODO
-		pass
-	print(r.text)
-	currencies = json.loads(r.text)
-	tmp = dict()
-	# tmp['date'] = datetime.datetime.strftime(datetime.datetime.today(), '%Y-%m-%d')
-	tmp['date'] = r.headers['Last-Modified']
-	# format '%a, %d %b %Y %H:%M:%S %Z'
-	tmp['base'] = 'USD'
-	tmp['rates'] = dict()
-	for k,v in currencies.items():
-		tmp['rates'][k] = {'name': v}
-	tmp['etag'] = r.headers['Etag']
-	url = URL.format('latest.json')
-	try:
-		r = requests.get(url)
-		r.raise_for_status()
-	except:
-		# TODO
-		pass
-	rates = json.loads(r.text)['rates']
-	for k,v in currencies.items():
-		tmp['rates'][k]['rate'] = rates[k]
-	try:
-		with open(cache_file, 'w', encoding='utf-8') as cache:
-			json.dump(tmp, cache, indent=4)
-	except IOError:
-		print('Unable to cache results.\n', file=sys.stderr)
-		# exit(1)
-		pass
-	return tmp
-
-def get_rates_current(overwrite = False):
-	url = URL.format('latest.json')
-	with open(cache_file, 'r', encoding='utf-8') as cache:
-		rates = json.load(cache)
-	try:
-		r = requests.get(url)
-		r.raise_for_status()
-	except ConnectionError:
-		print('Unable to update exchange rates.', file=sys.stderr)
-		return rates
-		exit(1)
-	except:
-		# TODO change this
-		print('Unable to update exchange rates.', file=sys.stderr)
-		return rates
-		exit(1)
-	rates['date'] = r.headers['Last-Modified']
-	_rates = r.json()
-	rates['base'] = _rates['base']
-	_rates = _rates['rates']
-	# TODO add functionality for help option
-	for k,v in _rates.items():
-		rates['rates'][k]['rate'] = v
-	try:
-		with open(cache_file, 'w', encoding='utf-8') as cache:
-			json.dump(rates, cache, indent=4)
-	except IOError:
-		print('Unable to cache results.\n', file=sys.stderr)
-		pass
-	return rates
-
-def get_rates():
-	try:
-		with open(cache_file, 'r', encoding='utf-8') as cache:
-			rates = json.load(cache)
-	except FileNotFoundError:
-		return create_cache()
-	time_since_last_update = datetime.datetime.today() - datetime.datetime.strptime(rates['date'], '%a, %d %b %Y %H:%M:%S %Z')
-	if time_since_last_update > datetime.timedelta(days=1):
-		print('Updating rates')
-		return get_rates_current()
-	return rates
-
-def convert(rates, from_curr, to_curr, val):
-	rate = 0
-	base = rates['base']
-	rates = rates['rates']
-	if from_curr == base:
-		try:
-			rate = rates[to_curr]['rate']
-		except KeyError:
-			print(f'Unknown currency: {to_curr}', file=sys.stderr)
-			exit(1)
-	elif from_curr not in rates:
-		print(f'Unknown currency: {from_curr}', file=sys.stderr)
-		exit(1)
-	if to_curr == base:
-		rate = 1 / rates[from_curr]['rate']
-	elif to_curr not in rates:
-		print(f'Unknown currency: {to_curr}', file=sys.stderr)
-		exit(1)
-	if not rate:
-		rate = rates[to_curr]['rate'] / rates[from_curr]['rate']
-	conv_rate = f'{val * rate :.2f}'
-	fmt = 'i' if to_curr == 'INR' else 'w'
-	conv_rate = conv(conv_rate, fmt)
-	if 'symbol' in rates[to_curr].keys():
-		conv_rate = rates[to_curr]['symbol'] + ' ' + conv_rate
-	return conv_rate
-
 if __name__ == '__main__':
-	# create 'data' folder if it doesn't already exist
-	Path(os.path.dirname(cache_file)).mkdir(parents=True, exist_ok=True)
 	args = parse_args()
-	converted = convert(get_rates(), *args)
+	converter = CurrencyExchange()
+	converted = converter.convert(*args)
 	print(converted)
